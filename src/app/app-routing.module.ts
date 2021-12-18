3ea63b3addb091509f5b799b3d59f76059042beb
TrangCH <camtrang@web.de>
2021-12-18 18:18:10 +0100

    start-screen: style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // path: '', Nothing, because it should be loaded at the beginning.
+  { path: '', component: StartScreenComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
