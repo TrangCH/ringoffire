ef63fd231972750ecfd3e697ec34cca753123e13
TrangCH <camtrang@web.de>
2021-12-19 23:57:02 +0100

    Object Game created, cards inserted, cards shuffled

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
@@ -7,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  game: Game | undefined; // Typ Game
 
   constructor() { }
 
   ngOnInit(): void {
+    this.newGame();
+    console.log(this.game);
+  }
+
+  newGame() {
+    this.game = new Game(); // Variable game bekommt ein neues Objekt erstellt, von dem was wir gerade angelegt haben.
   }
 
   takeCard() {
