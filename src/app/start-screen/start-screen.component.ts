02c11a0c41330efae7e28481a2556d7c79ddc2ff
TrangCH <camtrang@web.de>
2022-01-02 19:54:53 +0100

    Create a new game

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-start-screen',
@@ -10,14 +12,24 @@ export class StartScreenComponent implements OnInit {
 
   // private: router     Because we only need the router in this component.
   // public: router      If we want to use the router in HTML, then public.
-  constructor(private router: Router) { }
+  // Firestore importieren:
+  constructor(private firestore: AngularFirestore, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   newGame() {
+    let game = new Game(); // Neues Spiel anlegen
     // Start game
-    this.router.navigateByUrl('/game');
+    this.firestore // Zugriff auf firestore
+      .collection('games')  // Zugrifft auf die Sammlung 'games'
+      .add(game.toJSON()) // JSON hinzugefügt {'Hallo': 'Welt'} // Unser game Objekt wird in ein JSON umgewandelt.
+      // then nur einmal aufrufen. Macht Sinn, da nur jeweils ein neues Spiel.
+      .then((gameInfo: any) => { // .then Methode kann nur einmal aufgerufen werden. Funktioniert genauso wie .subscribe (kann mehrfach aufgerufen werden)
+        console.log(gameInfo); // gameInfo.id: hier
+        this.router.navigateByUrl('/game/' + gameInfo.id); 
+      });    
+     
   }
 
 }
